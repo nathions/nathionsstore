5655cf63aa54538b5208e3fbfe3bb33805501fbd
nathions <ntrboss@gmail.com>
2022-07-22 08:18:10 -0300

    Update produtos.php

--- produtos.php
+++ produtos.php
@@ -7,17 +7,17 @@ include_once './includes/_dados.php';
 <div class="container">
     <h2 style="margin-top: 1cm ;">PRODUTOS</h2>
 
-    <div class="row mt-5">
+    <div  class="row mt-3">
         <?php
-        foreach ($produtos as $key => $value) {
+
+
+        for ($i = 0; $i < 12; $i++) {
         ?>
 
-            <div class="card m-3" style="width: 15rem;">
-                <img src="./content/<?php echo $value['imagem']; ?>" class="card-img-top" alt="...">
+            <div class="card m-3" style="width: 15rem; height: 30rem;">
+                <img style="widht: 50px; height: 250px;" src="./content/<?php echo $produtos[$i]['imagem'] ?>" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <b>
-                        <p class="card-title"><?php echo $value['nome']; ?></p>
-                    </b>
+                    <b><p class="card-title"><?php echo $produtos[$i]['nome'] ?></p></b>
                 </div>
                 <div>
                     <a style="background-color: rgb(252,20,20); border: none; margin: 5px;" href="produto-detalhe.php?id=<?php echo $i; ?>" class="btn btn-primary">Comprar</a>
